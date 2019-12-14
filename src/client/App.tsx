83b3d739849c3ab47739337f980118eecfc32280
Sallem Ahmed <asallem94@gmail.com>
2019-12-14 13:01:01 -0500

    import styles and hook exam

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import Exam from './Exam';
+import './index.css';
 
 export const App = () => (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1>Lucid</h1>
-        <h2>Welcome to UI Team code assessment!</h2>
-    </div>
+	<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+		<h1>Lucid</h1>
+		<h2>Welcome to UI Team code assessment!</h2>
+		<Exam />
+	</div>
 );
